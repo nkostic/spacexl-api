d563caa34e2c0acc74eab000ee83dddde45599be
Nenad Kostic <nenadkostic@gmail.com>
2020-01-28 22:15:23 -0700

    API Improvements

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const mountRoutes = require("./routes");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
-
 const port = process.env.PORT;
-
 const app = express();
-// app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// );
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(helmet());
 app.use(cors());
 mountRoutes(app);
-
 app.listen(port, () =>
-  console.log(`Example RESTfull api runs on port ${port}.`)
+  console.log(`SpaceX Launches api runs on port ${port}.`)
 );
--- routes/flights.js
+++ routes/flights.js
@@ -4,50 +4,38 @@ const db = require("../db");
 
 const router = new Router();
 
-router.get("/land/:land/reused/:reused/with/:with", async (req, res) => {
-
-	const queryParams = [];
-	let queryWhereClause = ``;
-	let index = 0;
-	for (const prop in req.params) {
-		if(req.params[prop]=='true'){
-			index++;
-			queryParams.push(true);
-			if(prop=='land'){
-				queryWhereClause = `${queryWhereClause} and r.land_success = $${index}`
-			}
-			if(prop=='reused'){
-				queryWhereClause = `${queryWhereClause} and r.reused = $${index}`
-			}
-			if(prop=='with'){
-				queryWhereClause = `${queryWhereClause} and l.reddit_launch = $${index}`
-			}
-		}
-	}
-	
-	const querry =   `select
-		f.id,
-		l.mission_patch_small,
-		r.rocket_name,
-		r.rocket_type,
-		f.launch_date,
-		f.details,
-		f.flight_number,
-		l.article_link,
-		r.land_success,
-		r.reused,
-		l.reddit_launch
-	from
-		flights f
-		inner join rockets r on r.id = f.rocket_id
-		inner join links l on l.flight_id = f.id
-	where
-		1=1
-		${queryWhereClause}
-	order by f.launch_date desc;
+router.post("/", async (req, res) => {
+  const { filters } = req.body;
+  const querry = `
+		select
+			f.id,
+			l.mission_patch_small,
+			r.rocket_name,
+			r.rocket_type,
+			f.launch_date,
+			f.details,
+			f.flight_number,
+			l.article_link,
+			r.land_success,
+			r.reused,
+			l.reddit_launch
+		from
+			flights f
+			inner join rockets r on r.id = f.rocket_id
+			inner join links l on l.flight_id = f.id
+		where
+			($1 = false or r.land_success = $1)
+			and ($2 = false or r.reused = $2)
+			and ($3 = false or l.reddit_launch = $3)
+			
+		order by f.launch_date desc;
 	`;
 
-  const { rows } = await db.query(querry, queryParams);
+  const { rows } = await db.query(querry, [
+    filters.land,
+    filters.reused,
+    filters.with
+  ]);
   res.send(rows);
 });
 
